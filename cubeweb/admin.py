05f74926dcaa0255c0ad9c36e36f0bc535d26569
foresterh <foresterh@gmail.com>
2012-01-25 20:42:58 -0800

    not sure

---
+++ cubeweb/admin.py
@@ -0,0 +1,5 @@
+from cubeweb.models import CubeItem
+from django.contrib import admin
+
+
+admin.site.register(CubeItem)
--- cubeweb/models.py
+++ cubeweb/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
 class CubeItem(models.Model):
-    cubeValue = models.CharField(max_length=100)
-    keyName = models.CharField(max_length=100)
+    cubeValue = models.CharField(max_length=100, unique=True)
+
     parentKey = models.CharField(max_length=100)
     cubeType = models.CharField(max_length=100)
 
@@ -11,5 +13,11 @@ class CubeItem(models.Model):
         verbose_name = ('CubeItem')
         verbose_name_plural = ('CubeItems')
 
+    def make_uuid():
+        return str(uuid.uuid4())
+
+    keyName = models.CharField(max_length=36, primary_key=True,
+      default=make_uuid, editable=False)
+
     def __unicode__(self):
-        return __unicode__(self.cubeValue)
+        return unicode(self.cubeValue)
